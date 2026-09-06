eb896a811a24d59ca6b691ff19ffc3bab79c2599
Dvodnenko <thekovalskii@gmail.com>
2026-09-06 16:33:11 +0300

    fix actions' indices

--- src/mdp/main.py
+++ src/mdp/main.py
@@ -19,10 +19,10 @@ class GridWorld:
         states: np.ndarray, # 5x5 matrix
         policy: Policy,
         actions: dict = {
-            0: np.array([0, 1]), #  north/up
-            1: np.array([0, -1]), # south/down
-            2: np.array([1, 0]), #  east/right
-            3: np.array([-1, 0]) #  west/left
+            1: np.array([0, 1]), #  north/up
+            2: np.array([0, -1]), # south/down
+            3: np.array([1, 0]), #  east/right
+            4: np.array([-1, 0]) #  west/left
         },
         current_state: np.ndarray = np.array([0,0])
     ):
