5220fdf87301f94ff62c02408ffddbc83ed6344c
Dvodnenko <thekovalskii@gmail.com>
2026-09-06 16:58:45 +0300

    fix actions' vectors

--- src/mdp/main.py
+++ src/mdp/main.py
@@ -1,6 +1,8 @@
 # Implementation of the grid world
 # problem from S&B
 
+import time
+
 import numpy as np
 
 
@@ -19,10 +21,10 @@ class GridWorld:
         states: np.ndarray, # 5x5 matrix
         policy: Policy,
         actions: dict = {
-            1: np.array([0, 1]), #  north/up
-            2: np.array([0, -1]), # south/down
-            3: np.array([1, 0]), #  east/right
-            4: np.array([-1, 0]) #  west/left
+            1: np.array([-1, 0]), #  north/up
+            2: np.array([1, 0]), #  south/down
+            3: np.array([0, 1]), #  east/right
+            4: np.array([0, -1]) #  west/left
         },
         current_state: np.ndarray = np.array([0,0])
     ):
