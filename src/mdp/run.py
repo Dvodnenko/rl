0998f7dd0bec44d1a30047e092f742e94a711933
Dvodnenko <thekovalskii@gmail.com>
2026-09-08 16:45:16 +0300

    implement random walk with comments

--- src/mdp/run.py
+++ src/mdp/run.py
@@ -1,15 +1,29 @@
+import time
+
 import numpy as np
 
 from .main import GridWorld, Action
-
+from .policies import random_policy
 
 
 gw = GridWorld(
     np.arange(1, 26).reshape((5,5)),
-    policy=lambda: None,
-    cs=np.array([2, 2])
+    policy=random_policy,
+    cs=np.array([0, 0])
 )
 
 gw.move(Action.UP)
-print(gw.states)
-print(gw.cs, gw.value)
+
+
+t = 0
+while True:
+    time.sleep(1)
+    
+    S_t = gw.cs
+    A_t = gw.select_action()
+    print(f"t={t}, S_{t} = {S_t}, A_t {A_t.value}")
+    R_t1 = gw.move(A_t)
+    print(f"R_{t+1} = {"+" if R_t1 > 0 else ""}{R_t1}, S_{t+1} = {gw.cs}")
+    print()
+
+    t += 1
